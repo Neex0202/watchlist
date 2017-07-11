ea689b9e467abbec84748a196c450db1282d710e
[eddmoura] <edd.alca@gmail.com>
2017-07-11 15:01:25 -0400

    update

--- models/movies.js
+++ models/movies.js
@@ -1,31 +1,22 @@
 module.exports = function(sequelize, DataTypes){
-	var Movies = sequelize.define("Movie", {
-		movie_name: {
+	var Movie = sequelize.define("Movie", {
+		title: {
 			type: DataTypes.STRING,
-			allowNull: false
 		},
 		category: {
 			type: DataTypes.STRING,
-			allowNull: false
 		},
 
 		notes: {
 			type: DataTypes.STRING,
-			allowNull: true
 		},
 		imdb_id: {
 			type: DataTypes.STRING,
-			allowNull: false
 		}, 
 		poster: {
 			type: DataTypes.STRING,
-			allowNull: false
-		},
-		date: {
-			type: DataTypes.DATE, 
-			allowNull: false
 		}
 
 	});
-	return Movies;
+	return Movie;
 }
